e17c48ab35e5d05680781c3242a73d977c35ccb9
Nick Williams <nw14@sanger.ac.uk>
2020-12-08 11:52:56 +0000

    add install samtools

--- build/opt-build.sh
+++ build/opt-build.sh
@@ -28,6 +28,22 @@ INIT_DIR=`pwd`
 
 pip install --prefix=$INST_PATH pysam==0.9.0 PyPDF2==1.26.0 telomerehunter
 
+# Install samtools and hts-lib
+cd $INST_PATH
+mkdir $INST_PATH/bin
+wget https://github.com/samtools/htslib/releases/download/1.9/htslib-1.9.tar.bz2
+tar -vxjf htslib-1.9.tar.bz2
+cd htslib-1.9
+./configure --prefix=$INST_PATH
+make
+make install
+cd $INST_PATH
+wget https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2
+tar -vxjf samtools-1.9.tar.bz2
+cd samtools-1.9
+make
+cd $INIT_DIR
+export PATH=${INST_PATH}/bin:$PATH
 #Now fetch and install R 3.3.0
 curl -sSL --retry 10 https://cran.rstudio.com/src/base/R-3/R-3.3.0.tar.gz > R-3.3.0.tar.gz
 tar -zxf R-3.3.0.tar.gz
@@ -36,6 +52,9 @@ cd R-3.3.0
 make
 make check
 make install
+
+
+
 
 export R_LIBS=$INST_PATH/R-lib
 export R_LIBS_USER=$R_LIBS
